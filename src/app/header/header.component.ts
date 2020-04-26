4a329e5db9b6a6d3a59b06fc9020c1d5abbc6c50
chamaboy <hiroponpoko07@gmail.com>
2020-04-26 11:46:48 +0900

    feature(drawer): サイドナビゲーション開閉の機能追加

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { DrawerService } from '../services/drawer.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private drawerService: DrawerService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggle() {
+    this.drawerService.toggle();
   }
-
 }
