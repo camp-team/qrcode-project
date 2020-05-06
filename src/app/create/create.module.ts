9dd378c5f06e348a46e002c8b477f4daabae8e07
chamaboy <hiroponpoko07@gmail.com>
2020-05-06 13:15:48 +0900

    feat(form): 共通するformの実装 #36

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -4,9 +4,18 @@ import { CommonModule } from '@angular/common';
 import { CreateRoutingModule } from './create-routing.module';
 import { QrCodeComponent } from './qr-code/qr-code.component';
 import { FormComponent } from './form/form.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [QrCodeComponent, FormComponent],
-  imports: [CommonModule, CreateRoutingModule],
+  imports: [
+    CommonModule,
+    CreateRoutingModule,
+    SharedModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class CreateModule {}
