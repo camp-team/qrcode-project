013ba1672123fd697e00465a40f01a9991f29df5
chamaboy <hiroponpoko07@gmail.com>
2020-05-07 16:51:23 +0900

    feat(form): qrCode用の投稿画面追加 #38

--- src/app/create/form/form.component.ts
+++ src/app/create/form/form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -7,14 +13,24 @@ import { FormBuilder, Validators, FormControl } from '@angular/forms';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  form = this.fb.group({
-    name: ['', [Validators.required, Validators.maxLength(20)]],
-    image: ['', Validators.required],
-    point: ['', Validators.maxLength(5)],
-    addPoint: [''],
-    storeName: [''],
-    storeImage: [''],
-  });
+  // form = this.fb.group({
+  //   name: ['', [Validators.required, Validators.maxLength(20)]],
+  //   image: ['', Validators.required],
+  //   point: ['', Validators.maxLength(5)],
+  //   addPoint: [''],
+  //   storeName: [''],
+  //   storeImage: [''],
+  // });
+
+  form: FormGroup;
+  type;
+  customForm = {
+    qrCode: {
+      charge: [''],
+      autoCharge: [''],
+      availableCredit: [''],
+    },
+  };
 
   get nameControl() {
     return this.form.get('name') as FormControl;
@@ -24,9 +40,26 @@ export class FormComponent implements OnInit {
     return this.form.get('image') as FormControl;
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {
+    this.route.queryParamMap.subscribe((map) => {
+      this.type = map.get('type');
+      this.buildForm(this.type);
+    });
+  }
 
   ngOnInit(): void {}
+
+  buildForm(type) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(20)]],
+      image: ['', Validators.required],
+      point: ['', Validators.maxLength(5)],
+      addPoint: [''],
+      storeName: [''],
+      storeImage: [''],
+      ...this.customForm[type],
+    });
+  }
 
   getResult() {
     console.log(this.form.value);
