c7279c2ef853e90f183a7b4c3d0dcb862190a894
chamaboy <hiroponpoko07@gmail.com>
2020-05-12 19:56:25 +0900

    feat(github): githubAction修正 #46

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CreateRoutingModule } from './create-routing.module';
-import { QrCodeComponent } from './qr-code/qr-code.component';
 import { FormComponent } from './form/form.component';
 import { SharedModule } from '../shared/shared.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,7 +10,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  declarations: [QrCodeComponent, FormComponent],
+  declarations: [FormComponent],
   imports: [
     CommonModule,
     CreateRoutingModule,
