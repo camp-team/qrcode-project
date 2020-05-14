89578f92f8982ceefd0638fe2c837a416b08d7de
chamaboy <hiroponpoko07@gmail.com>
2020-05-14 14:44:28 +0900

    fix(code-card): 抜けていた情報追加 #45

--- src/app/interfaces/code-card.ts
+++ src/app/interfaces/code-card.ts
@@ -3,6 +3,7 @@ import { Card } from './card';
 export interface CodeCard extends Card {
   charge: string;
   autoCharge: string;
+  availableCredit: string;
   pushMoney: string;
   pullMoney: string;
 }
