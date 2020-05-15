dc576fd773c675ddde2b3b807685ce18067506b2
chamaboy <hiroponpoko07@gmail.com>
2020-05-15 09:56:13 +0900

    feat(code-detail): 詳細画面作成 #45

--- src/app/code-detail/code-detail.module.ts
+++ src/app/code-detail/code-detail.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CodeDetailRoutingModule } from './code-detail-routing.module';
 import { CodeDetailComponent } from './code-detail/code-detail.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CodeDetailComponent],
-  imports: [CommonModule, CodeDetailRoutingModule],
+  imports: [CommonModule, CodeDetailRoutingModule, SharedModule],
 })
 export class CodeDetailModule {}
