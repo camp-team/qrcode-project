c3c41fcfe4fe268dc91c9ba144531835a14b6602
chamaboy <hiroponpoko07@gmail.com>
2020-06-03 16:59:30 +0900

    fix(function): タイプミス修正 #53

--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -1,10 +1,10 @@
 import * as functions from 'firebase-functions';
-import * as adimin from 'firebase-admin';
+import * as admin from 'firebase-admin';
 import { UserData } from './interfaces/user';
 
-adimin.initializeApp();
+admin.initializeApp();
 
-const db = adimin.firestore();
+const db = admin.firestore();
 
 export const createUser = functions
   .region('asia-northeast1')
