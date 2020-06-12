2b2cbbcf8972be79fa5e278df59aec9b8d1950eb
chamaboy <hiroponpoko07@gmail.com>
2020-06-12 11:55:46 +0900

    refactor(card.service): 型の修正とupdate関数修正 #66

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -58,14 +58,16 @@ export class CardService {
   }
 
   getElectronCard(cardId: string): Observable<ElectronCard> {
-    return this.db.doc<ElectronCard>(`electornCards/${cardId}`).valueChanges();
+    return this.db.doc<ElectronCard>(`electronCards/${cardId}`).valueChanges();
   }
 
   async updateCodeCard(
     codeCard: Omit<CodeCard, 'imageURL'>,
     file?: File
   ): Promise<void> {
-    const data: any = {};
+    const data: {
+      imageURL?: string;
+    } = {};
     if (file) {
       const imageURL: string = await this.getUploadImageURL(
         codeCard.cardId,
@@ -75,10 +77,13 @@ export class CardService {
     }
     return this.db
       .doc(`codeCards/${codeCard.cardId}`)
-      .update({
-        ...data,
-        ...codeCard,
-      })
+      .set(
+        {
+          ...data,
+          ...codeCard,
+        },
+        { merge: true }
+      )
       .then(() => {
         console.log('データを編集しました');
       });
