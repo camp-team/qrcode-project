f8d76ca7ef6fd44c7aa9db715ab8e5f44c6c7401
chamaboy <hiroponpoko07@gmail.com>
2020-06-22 02:36:29 +0900

    feat(code-card): 検索結果の表示 #71

--- src/app/code-card/code-card/code-card.component.ts
+++ src/app/code-card/code-card/code-card.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CardService } from 'src/app/services/card.service';
 import { Observable } from 'rxjs';
 import { CodeCard } from '@interfaces/code-card';
+import { SearchService } from 'src/app/services/search.service';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-code-card',
@@ -10,8 +13,30 @@ import { CodeCard } from '@interfaces/code-card';
 })
 export class CodeCardComponent implements OnInit {
   codeCards$: Observable<CodeCard[]> = this.cardService.getCodeCards();
+  result: any;
 
-  constructor(private cardService: CardService) {}
+  constructor(
+    private cardService: CardService,
+    private searchService: SearchService,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParamMap.subscribe((param) => {
+      const searchQuery: string = param.get('searchQuery');
+      this.searchService.index.store.search(searchQuery).then((result) => {
+        this.result = result.hits;
+        console.log(this.result.map((store) => store.id));
+        return this.codeCards$.pipe(
+          map((codeCards) => {
+            return codeCards.filter((codeCard) => {
+              return codeCard.storeIds.find(
+                (id) => id === this.result.map((store) => store.id)
+              );
+            });
+          })
+        );
+      });
+    });
+  }
 
   ngOnInit(): void {}
 }
