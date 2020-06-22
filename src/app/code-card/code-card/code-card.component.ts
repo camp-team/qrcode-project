4241796c29ccc0c2ac51ca66bbdab28894df6c0e
chamaboy <hiroponpoko07@gmail.com>
2020-06-22 13:55:12 +0900

    fix(code-card): 検索結果の関数修正 #71

--- src/app/code-card/code-card/code-card.component.ts
+++ src/app/code-card/code-card/code-card.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { CodeCard } from '@interfaces/code-card';
 import { SearchService } from 'src/app/services/search.service';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-code-card',
@@ -24,13 +24,23 @@ export class CodeCardComponent implements OnInit {
       const searchQuery: string = param.get('searchQuery');
       this.searchService.index.store.search(searchQuery).then((result) => {
         this.result = result.hits;
-        console.log(this.result.map((store) => store.id));
+        const resultIds = this.result.map((store) => store.id);
+        console.log(resultIds);
+        console.log(resultIds.find((resultId) => resultId === 'naturallawson'));
         return this.codeCards$
           .pipe(
+            tap((cards) => {
+              const ids = cards.map((card) => card.storeIds);
+              console.log(ids[2]);
+              const naturallawson = ids[2].find(
+                (storeId) => storeId === 'naturallawson'
+              );
+              console.log(naturallawson);
+            }),
             map((codeCards) => {
               return codeCards.filter((codeCard) => {
-                return codeCard.storeIds.find(
-                  (id) => id === this.result.map((store) => store.id)
+                return codeCard.storeIds.find((id) =>
+                  resultIds.find((resultId) => resultId === id)
                 );
               });
             })
