16c2641ea14c33ce6ea4b02d44aace06ed9b98e2
chamaboy <hiroponpoko07@gmail.com>
2020-06-23 13:23:48 +0900

    feat(electron-card): 検索結果の表示 #71

--- src/app/electron-card/electron-card/electron-card.component.ts
+++ src/app/electron-card/electron-card/electron-card.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CardService } from 'src/app/services/card.service';
 import { Observable } from 'rxjs';
 import { ElectronCard } from '@interfaces/electron-card';
+import { SearchService } from 'src/app/services/search.service';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-electron-card',
@@ -12,8 +15,31 @@ export class ElectronCardComponent implements OnInit {
   electronCards$: Observable<
     ElectronCard[]
   > = this.cardService.getElectronCards();
+  result: any;
+  filteredCards$: Observable<ElectronCard[]>;
 
-  constructor(private cardService: CardService) {}
+  constructor(
+    private cardService: CardService,
+    private searchService: SearchService,
+    private router: ActivatedRoute
+  ) {
+    this.router.queryParamMap.subscribe((param) => {
+      const searchQuery: string = param.get('searchQuery');
+      this.searchService.index.store.search(searchQuery).then((result) => {
+        this.result = result.hits;
+        const resultIds: string[] = this.result.map((store) => store.id);
+        return (this.filteredCards$ = this.electronCards$.pipe(
+          map((electronCards) => {
+            return electronCards.filter((electronCard) => {
+              return electronCard.storeIds.find((id) =>
+                resultIds.find((resultId) => resultId === id)
+              );
+            });
+          })
+        ));
+      });
+    });
+  }
 
   ngOnInit(): void {}
 }
