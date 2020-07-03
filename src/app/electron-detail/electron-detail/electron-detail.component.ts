b8f52b5e10902f903e57b925157165dd2d45048f
chamaboy <hiroponpoko07@gmail.com>
2020-07-04 00:21:51 +0900

    feat(detail): 詳細画面からの検索処理追加 #83

--- src/app/electron-detail/electron-detail/electron-detail.component.ts
+++ src/app/electron-detail/electron-detail/electron-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CardService } from 'src/app/services/card.service';
 import { Observable, observable } from 'rxjs';
 import { ElectronCard } from '@interfaces/electron-card';
 import { switchMap } from 'rxjs/operators';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-electron-detail',
@@ -12,16 +13,29 @@ import { switchMap } from 'rxjs/operators';
 })
 export class ElectronDetailComponent implements OnInit {
   cardId: string;
+  searchQuery: string;
   electronCard$: Observable<ElectronCard> = this.route.paramMap.pipe(
-    switchMap((param) => {
-      this.cardId = param.get('id');
+    switchMap((map) => {
+      this.cardId = map.get('id');
       return this.cardService.getElectronCard(this.cardId);
     })
   );
   constructor(
     private route: ActivatedRoute,
-    private cardService: CardService
-  ) {}
+    private cardService: CardService,
+    private router: Router
+  ) {
+    this.route.queryParamMap.subscribe((param) => {
+      this.searchQuery = param.get('searchQuery');
+      if (this.searchQuery) {
+        this.router.navigate(['electron-card'], {
+          queryParams: {
+            searchQuery: this.searchQuery,
+          },
+        });
+      }
+    });
+  }
 
   ngOnInit(): void {}
 }
