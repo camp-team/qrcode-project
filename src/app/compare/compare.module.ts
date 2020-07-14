082f2472934ca48ea96f69c0d49e2b9c5844b045
chamaboy <hiroponpoko07@gmail.com>
2020-07-14 11:43:58 +0900

    feat(compare): 選択での画面遷移 #85

--- src/app/compare/compare.module.ts
+++ src/app/compare/compare.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CompareRoutingModule } from './compare-routing.module';
 import { CompareComponent } from './compare/compare.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [CompareComponent],
-  imports: [CommonModule, CompareRoutingModule],
+  imports: [CommonModule, CompareRoutingModule, SharedModule, MatSelectModule],
 })
 export class CompareModule {}
--- src/app/compare/compare/compare.component.ts
+++ src/app/compare/compare/compare.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CardService } from 'src/app/services/card.service';
+import { Observable } from 'rxjs';
+import { CodeCard } from '@interfaces/code-card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-compare',
@@ -6,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compare.component.scss'],
 })
 export class CompareComponent implements OnInit {
-  constructor() {}
+  codeCards$: Observable<CodeCard[]> = this.cardService.getCodeCards();
+
+  constructor(private cardService: CardService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  navigate(cardId: string) {
+    this.router.navigate([], {
+      queryParamsHandling: 'merge',
+      queryParams: {
+        cardId,
+      },
+    });
+  }
 }
