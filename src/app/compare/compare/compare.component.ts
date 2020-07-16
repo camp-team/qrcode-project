662a7c532cdb9696471906478792a7977176ab10
chamaboy <hiroponpoko07@gmail.com>
2020-07-16 15:16:21 +0900

    feat(compare): 比較の実装 #85

--- src/app/compare/compare/compare.component.ts
+++ src/app/compare/compare/compare.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CardService } from 'src/app/services/card.service';
 import { Observable } from 'rxjs';
 import { CodeCard } from '@interfaces/code-card';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ElectronCard } from '@interfaces/electron-card';
 
 @Component({
   selector: 'app-compare',
@@ -10,17 +11,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./compare.component.scss'],
 })
 export class CompareComponent implements OnInit {
+  cardId1: string;
+  cardId2: string;
+  cardId3: string;
   codeCards$: Observable<CodeCard[]> = this.cardService.getCodeCards();
+  selectedCard$: Observable<CodeCard>;
+  selectedCards$: Observable<CodeCard>[] = [];
+  paramaters: string[] = [];
 
-  constructor(private cardService: CardService, private router: Router) {}
+  indexes = [...Array(3)].map((_, i) => i);
+
+  constructor(
+    private cardService: CardService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParamMap.subscribe((param) => {
+      this.cardId1 = param.get('cardId1');
+      this.cardId2 = param.get('cardId2');
+      this.cardId3 = param.get('cardId3');
+      this.selectedCards$[0] = this.cardService.getCodeCard(this.cardId1);
+      this.selectedCards$[1] = this.cardService.getCodeCard(this.cardId2);
+      this.selectedCards$[2] = this.cardService.getCodeCard(this.cardId3);
+    });
+  }
 
   ngOnInit(): void {}
 
-  navigate(cardId: string) {
+  navigate(cardId: string, index: number) {
+    console.log(index);
+    switch (index) {
+      case 0:
+        this.cardId1 = cardId;
+        break;
+      case 1:
+        this.cardId2 = cardId;
+        break;
+      case 2:
+        this.cardId3 = cardId;
+        break;
+    }
     this.router.navigate([], {
       queryParamsHandling: 'merge',
       queryParams: {
-        cardId,
+        cardId1: this.cardId1,
+        cardId2: this.cardId2,
+        cardId3: this.cardId3,
       },
     });
   }
