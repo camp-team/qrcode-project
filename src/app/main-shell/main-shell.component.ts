af9624a2b5e92e8fc545ca9ed6cbf16246028042
chamaboy <hiroponpoko07@gmail.com>
2020-07-17 16:22:32 +0900

    fix(sticky): fixedに変更 #85

--- src/app/main-shell/main-shell.component.ts
+++ src/app/main-shell/main-shell.component.ts
@@ -3,8 +3,6 @@ import { DrawerService } from '../services/drawer.service';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { RouterService } from '../services/router.service';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-shell',
@@ -14,35 +12,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class MainShellComponent implements OnInit {
   isOpened$: Observable<boolean> = this.drawerService.isOpened$;
   isMobile$: Observable<boolean>;
-  isSticky$: Observable<boolean> = this.routerService.isSticky$;
 
   constructor(
     private drawerService: DrawerService,
-    private breakpointObserver: BreakpointObserver,
-    private routerService: RouterService,
-    private route: ActivatedRoute,
-    private router: Router
+    private breakpointObserver: BreakpointObserver
   ) {
-    this.route.paramMap.subscribe((param) => {
-      const cardId = param.get('/code-detail/:id');
-      console.log(cardId);
-    });
     this.isMobile$ = this.breakpointObserver
       .observe(Breakpoints.XSmall)
       .pipe(map((result) => result.matches));
   }
 
   ngOnInit(): void {}
-
-  navigate() {
-    this.route.paramMap.subscribe((paramMap) => {
-      const id = paramMap.get('id');
-      console.log(id);
-      this.router.navigate(['/compare'], {
-        queryParams: {
-          cardId1: id,
-        },
-      });
-    });
-  }
 }
