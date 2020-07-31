ea17b714e879ef47f9f67fbba00fd0f62e7d1a7f
chamaboy <hiroponpoko07@gmail.com>
2020-07-31 12:00:00 +0900

    feat(customer): 顧客データの取得 #72

--- functions/src/stripe/customer.function.ts
+++ functions/src/stripe/customer.function.ts
@@ -17,3 +17,21 @@ export const createStripeCustomer = functions
       customerId: customer.id,
     });
   });
+
+export const getStripeCustomer = functions
+  .region('asia-northeast1')
+  .https.onCall(async (data, context) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError('permission-denied', 'not user');
+    }
+    const customer = (
+      await db.doc(`customer/${context.auth.uid}`).get()
+    ).data();
+    if (!customer) {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        'there is no customer'
+      );
+    }
+    return stripe.customers.retrieve(customer.customerId);
+  });
