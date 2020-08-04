e8dc9c1cc5cb25deb0629d41b6bdac9afb97ac12
chamaboy <hiroponpoko07@gmail.com>
2020-08-04 13:22:55 +0900

    feat(stripe): setupintentの型指定 #72

--- functions/src/stripe/intent.function.ts
+++ functions/src/stripe/intent.function.ts
@@ -6,24 +6,26 @@ import { Customer } from '../interfaces/customer';
 
 export const createStripeSetupIntent = functions
   .region('asia-northeast1')
-  .https.onCall(async (_, context) => {
-    if (!context.auth) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        '認証エラーが発生しました。'
-      );
+  .https.onCall(
+    async (_, context): Promise<Stripe.SetupIntent> => {
+      if (!context.auth) {
+        throw new functions.https.HttpsError(
+          'permission-denied',
+          '認証エラーが発生しました。'
+        );
+      }
+      const cusomer: Customer = (
+        await db.doc(`customer/${context.auth.uid}`).get()
+      ).data() as Customer;
+      if (!cusomer) {
+        throw new functions.https.HttpsError(
+          'permission-denied',
+          'プラットフォームにカスタマーが存在しません。'
+        );
+      }
+      return stripe.setupIntents.create({
+        payment_method_types: ['card'],
+        customer: cusomer.customerId,
+      });
     }
-    const cusomer: Customer = (
-      await db.doc(`customer/${context.auth.uid}`).get()
-    ).data() as Customer;
-    if (!cusomer) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        'プラットフォームにカスタマーが存在しません。'
-      );
-    }
-    return stripe.setupIntents.create({
-      payment_method_types: ['card'],
-      customer: cusomer.customerId,
-    });
-  });
+  );
