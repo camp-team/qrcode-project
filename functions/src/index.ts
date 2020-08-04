d298881feddf3c347b27e031900df5f346357683
chamaboy <hiroponpoko07@gmail.com>
2020-08-04 17:10:10 +0900

    feat(stripe): index.tsの定義 #72

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,3 +4,5 @@ export { deleteUser } from './user.function';
 export { deleteUserData } from './user.function';
 export { createStripeCustomer } from './stripe/customer.function';
 export { getStripeCustomer } from './stripe/customer.function';
+export { createStripeSetupIntent } from './stripe/intent.function';
+export { setStripePaymentMethod } from './stripe/payment-method.function';
