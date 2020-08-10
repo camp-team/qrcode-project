24cf4ec70f8f978cd69d0dcadcc76f029d12955e
chamaboy <hiroponpoko07@gmail.com>
2020-08-10 17:24:57 +0900

    feat(payment): 支払い画面追加 #72

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import Stripe from 'stripe';
+import { PaymentService } from '../services/payment.service';
+import { MatDialog } from '@angular/material/dialog';
+import { RegisterCardDialogComponent } from '../shared/register-card-dialog/register-card-dialog.component';
 
 @Component({
   selector: 'app-payment',
@@ -6,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent implements OnInit {
-  constructor() {}
+  paidAds: boolean;
+  paymentCard: Stripe.PaymentMethod;
+  constructor(
+    private paymentService: PaymentService,
+    private dialog: MatDialog
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.paymentService.getPaymentMethod().then((cards) => {
+      this.paymentCard = cards.data[0];
+    });
+  }
+
+  openRegisterCardDialog() {
+    this.dialog.open(RegisterCardDialogComponent);
+  }
+
+  charge() {
+    this.paymentService.charge();
+  }
 }
--- src/app/payment/payment.module.ts
+++ src/app/payment/payment.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PaymentRoutingModule } from './payment-routing.module';
 import { PaymentComponent } from './payment.component';
+import { MatStepperModule } from '@angular/material/stepper';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PaymentComponent],
-  imports: [CommonModule, PaymentRoutingModule],
+  imports: [CommonModule, PaymentRoutingModule, MatStepperModule, SharedModule],
 })
 export class PaymentModule {}
