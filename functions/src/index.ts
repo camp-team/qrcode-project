b26852b7af2be149c7cd515009b8a5ae5fc6d926
chamaboy <hiroponpoko07@gmail.com>
2020-08-10 17:18:07 +0900

    feat(index): export追加 #72

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,3 +8,4 @@ export { deleteStripeCustomer } from './stripe/customer.function';
 export { createStripeSetupIntent } from './stripe/intent.function';
 export { setStripePaymentMethod } from './stripe/payment-method.function';
 export { getStripePaymentMethod } from './stripe/payment-method.function';
+export { payStripeProduct } from './stripe/charge.function';
