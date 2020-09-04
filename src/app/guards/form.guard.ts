671da107fe0f1a334f27bb99a570a8f508d3a859
chamaboy <hiroponpoko07@gmail.com>
2020-09-04 14:37:29 +0900

    refacto(form.guard): 不要なコード修正 #81

--- src/app/guards/form.guard.ts
+++ src/app/guards/form.guard.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  CanDeactivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { CanDeactivate } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { FormComponent } from '../create/form/form.component';
 
